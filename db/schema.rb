c351edda431385531f8f40d627f71ff3d014452a
Jon Karp <jonkar@gmail.com>
2015-08-25 16:06:22 -0400

    recovery from lost data

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20150813192306) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "proteins", force: :cascade do |t|
     t.string   "name"
     t.string   "gi_number"
